87194d54b1874bd1ed42986e414f37566a5acd97
benrkia <fatimazahrae.benrkia@gmail.com>
2023-03-16 19:10:08 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <unisdt.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Description: print a quote using write function
- *   ssize_t write(int fd, const void *buf.size_t count);
+ *      write(int fd, const void *buf.size_t count);
  *
  * Return: 1 (not success)
- */
+*/
 
 int main(void)
 {
