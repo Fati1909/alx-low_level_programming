0e6f8094cd1f6925ee524134de91689e7c03edc3
benrkia <fatimazahrae.benrkia@gmail.com>
2023-04-05 12:49:48 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _puts_recursion - function that prints a string
  * @s: pointer
- * Return: nothing
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '/0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
