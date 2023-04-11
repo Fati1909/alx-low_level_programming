8219cf665a74d285ab535bd90d7475c37fcc7e88
benrkia <fatimazahrae.benrkia@gmail.com>
2023-04-11 07:20:27 -0700

    B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strdup - function that returns a pointer
+ * to a newly allocated space in memory
+ * @str: string
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	int n = 0, size = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	ptr = malloc(size * sizeof(*str) + 1);
+
+	if (ptr == 0)
+		return (NULL);
+	else
+	{
+		for (; n < size; n++)
+			ptr[n] = str[n];
+	}
+	return (ptr);
+}
