c54619c3d13c407d0b78ce6b800e9f8b74f73631
benrkia <fatimazahrae.benrkia@gmail.com>
2023-04-13 16:36:24 -0700

    R

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+:
+/**
+ * *_memset - description
+ * @s: pointer
+ * @b: constant
+ * @n: num
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - function that allocates memory for an array
+ * @nmemb: array
+ * @size: size
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
