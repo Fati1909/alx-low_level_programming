cae5689f129b22e937a64504e44671945bf53077
benrkia <fatimazahrae.benrkia@gmail.com>
2023-05-01 12:48:41 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		n++;
+		i++;
 	}
-	return (n);
+	return (i);
 }
