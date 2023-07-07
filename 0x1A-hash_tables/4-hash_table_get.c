f0c639bd0b175e8e37a41f1792f7f44d73d530b8
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-07 04:57:01 -0700

    Hash_Tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value 
+ * associated with a key
+ * @ht: pointer to hash table
+ * @key: key to retrive value
+ * Return: value associated with the element
+ * or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0;
+	hash_node_t *bucket;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[idx];
+
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+			return (bucket->value);
+		bucket = bucket->next;
+	}
+	return (NULL);
+}
